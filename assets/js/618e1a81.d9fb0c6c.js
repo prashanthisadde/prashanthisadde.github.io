7065bde93eadafbf3eeea480d7c1a3d2a44246aa
Prashanthi Sadde <prashanthi@Prashanthis-MacBook-Pro.local>
2022-06-28 10:45:41 +0530

    Deploy website - based on abd25eb425a92d2a5070cd8ff4994572ba143e1c

---
+++ assets/js/618e1a81.d9fb0c6c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkta_web=self.webpackChunkta_web||[]).push([[3627],{8035:function(e){e.exports=JSON.parse('{"permalink":"/notions","page":1,"postsPerPage":10,"totalPages":1,"totalCount":10,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
